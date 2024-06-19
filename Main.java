d13290c3de758932aa61f4ce8811aaa13d17f24d
mangalaraj24 <rajkishore2416@gmail.com>
2024-06-19 21:33:24 +0530

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-package AirlineManagementSystem;
+ckage AirlineManagementSystem;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -14,17 +15,19 @@ public class Main {
 
 		int choice = 0;
 		do {
-			System.out.println("Welcome to Airline Management System");
-			System.out.println("1 -> Add new passenger");
-			System.out.println("2 -> Edit Passenger");
-			System.out.println("3 -> Quit the Airline System.");
+			System.out.println(" Welcome to Airline Management System");
+			System.out.println("Press 1 -> Add new passenger");
+			System.out.println("Press 2 -> Edit Passenger");
+			System.out.println("Press 3 -> List of Passengers");
+			System.out.println("Press 4 -> Delete Passengers");
+			System.out.println("Press 5 -> Quit the Airline System.");
 			System.out.println("Enter your choice...");
 
 			try {
 				choice = s.nextInt();
 				switch (choice) {
 				case 1:
-					PassengersController.addNewPassenger(database);
+					passengersController.addNewPassenger();
 					break;
 
 				case 2:
@@ -35,26 +38,44 @@ public class Main {
 
 					System.out.println("Current Details");
 					Passenger passenger = database.getPassengerById(id);
-					PassengersController.editPassenger(passenger);
+					passengersController.editPassenger(passenger);
 					break;
+					
 				case 3:
+					
+					System.out.println("Passengers List are,");
+					ArrayList<Passenger> passengerList=new ArrayList<Passenger>();
+					passengerList.addAll(database.getAllPassengers());
+					for(Passenger p: passengerList) {
+						System.out.println(p.toString());
+					}
+					break;
+					
+				case 4:
+					
+					System.out.println("Enter the Passenger Id : ");
+					int pid=s.nextInt();
+					passengersController.deletePassengerById(pid);
+					break;
+					
+				case 5:
 
-					System.out.println("3 - Quit");
+					System.out.println("4 - Quit");
 					break;
 
 				default:
 					System.out.println("Invalid choice. Please enter a valid option.");
 				}
 			} catch (NoSuchElementException e) {
-				System.out.println("Error reading input: " + e.getMessage());
+				System.err.println("Error reading input: " + e.getMessage());
 				s.nextLine(); 
 			} catch (SQLException e) {
-				System.out.println("Database error: " + e.getMessage());
+				System.err.println("Database error: " + e.getMessage());
 			} catch (Exception e) {
-				System.out.println("An unexpected error occurred: " + e.getMessage());
+				System.err.println("An unexpected error occurred: " + e.getMessage());
 			}
 
-		} while (choice != 3);
+		} while (choice != 6);
 
 		s.close();
 
